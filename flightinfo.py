472949a364e78d1f81fac38cb674788117df1f98
Mineev <mineevahmed@gmail.com>
2016-06-17 22:36:17 +0800

    fix bags

--- flightinfo.py
+++ flightinfo.py
@@ -234,8 +234,7 @@ if __name__ == '__main__':
     departures.getfromserver(*departxmlreqchart)
     departures.sort(key=getflighttime)
     arrivals.sort(key=getflighttime)
-    arrivals.save('arrivals.pkl')
-    departures.save('departures.pkl')
+
     departurepicklefile = 'departures.pkl'
     arrivalspicklefile = 'arrivals.pkl'
     internalftp = ('172.17.10.120', 'admin', '34652817')
