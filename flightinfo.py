4b14179f4237f295d85107fa0673308094c5c99d
Mineev <mineevahmed@gmail.com>
2016-06-21 08:47:00 +0800

    fix bags

--- flightinfo.py
+++ flightinfo.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+import sys
 from xml.etree.ElementTree import parse
-from http.client import HTTPConnection
 import datetime as DT
 from ftplib import FTP
 import pickle
+if int(sys.version_info[0]) == 3:
+    from http.client import HTTPConnection
+else:
+    raise Exception("Version python less then 3")
 
 class FlightsInfo(dict):
     '''Structure: {'FLY': {STATUSCHECKIN: , STATUSBOARD: , STATUSBAG,
@@ -12,26 +16,48 @@ class FlightsInfo(dict):
         self = {}
 
     def updatefromflightes(self, flights):
-        flynew = {'STATCHECKIN': False, 'STATBOARD': False, 'STATBAGG': False,
+        flynewinfo = {'STATCHECKIN': False, 'STATBOARD': False, 'STATBAGG': False,
                   'CHECKINS': '', 'GATE': '', 'BAGGAGE': ''}
         for flight in flights:
             fly = flight['FLY']
             if fly in self:
                 continue
             else:
-                self.setdefault(fly, flynew)
-        isnotflights = True
-        tmplst = []
+                self.setdefault(fly, flynewinfo)
+        currentflylst = []
         for flight in flights:
-            tmplst.append(flight['FLY'])
-
-
+            currentflylst.append(flight['FLY'])
+        for fly in self:
+            if fly not in currentflylst:
+                self.pop(fly)
 
     def __str__(self):
         st = ''
         for flightinfo in self:
             st += str(flightinfo) + ': ' + str(self[flightinfo]) + '\n'
         return st
+
+    def save(self, filename):
+        f = open(filename, 'wb')
+        pickle.dump(self, f)
+
+    def load(self, filename):
+        try:
+            f = open(filename, 'rb')
+        except FileNotFoundError:
+            return False
+        self = pickle.load(f)
+        return True
+
+    def getflightstatus(self, fly, param):
+        '''param mast be STATCHECIN, STATBAGG, STATBOARD, GATE, CHECKINS, BAGGAGE'''
+        params = ['STATCHECKIN', 'STATBOARD', 'STATBAGG', 'CHECKINS', 'GATE', 'BAGGAGE']
+        if param not in params:
+            raise Exception('Parametr not in permissions list')
+        if fly in self:
+            return self[fly][param]
+        else:
+            return None
 
 class Flights(list):
     '''Info about air flight. Structure:
@@ -47,82 +73,100 @@ class Flights(list):
 
     def __init__(self):
         self = []
-    def getfromserver(self, server, port, request):
-        '''запросить xml и сохранить в файл'''
-        filename = "tmpxmlstructure.xml"
-        conector = HTTPConnection(server, port)
-        conector.request("GET", request)
-        result = conector.getresponse()
-        file = open(filename, "w")
-        file.write(result.read().decode("utf-8"))
-        file.close()
-        conector.close()
+
+    def getfromxml(self, filename):
         '''начать парсить xml файл в удобную структуру'''
-        reiseInfo = {}
+        flightinfo = {}
         tree = parse(filename)
         for fly in tree.findall('FLY'):
-            reiseInfo['FLY'] = fly.attrib['number']
+            flightinfo['FLY'] = fly.attrib['number']
             airportdist = ''
-            distinetion = ''
-            for ReiseElem in fly.getchildren():
-                if ReiseElem.tag.find('PORTDIST') != -1:
-                    if ReiseElem.text is not None:
+            destinetion = ''
+            for flightparam in fly.getchildren():
+                if flightparam.tag.find('PORTDIST') != -1:
+                    if flightparam.text is not None:
                         if airportdist == '':
-                            airportdist = ReiseElem.text
+                            airportdist = flightparam.text
                         else:
-                            airportdist += ' - ' + ReiseElem.text
+                            airportdist += ' - ' + flightparam.text
                     continue
-                if ReiseElem.tag.find('PUNKTDIST') != -1:
-                    if ReiseElem.text is not None:
-                        if distinetion == '':
-                            distinetion = ReiseElem.text
+                if flightparam.tag.find('PUNKTDIST') != -1:
+                    if flightparam.text is not None:
+                        if destinetion == '':
+                            destinetion = flightparam.text
                         else:
-                            distinetion +=  ' - '+ ReiseElem.text
+                            destinetion +=  ' - '+ flightparam.text
                     continue
-                reiseInfo.setdefault(ReiseElem.tag, ReiseElem.text)
-            reiseInfo['PORTDIST'] = airportdist
-            reiseInfo['PUNKTDIST'] = distinetion
-            #перевести время
-            if reiseInfo['TPLAN'] is None or reiseInfo['DPLAN'] is None:
-                reiseInfo['TIMEPLAN'] = None
+                flightinfo.setdefault(flightparam.tag, flightparam.text)
+            flightinfo['PORTDIST'] = airportdist
+            flightinfo['PUNKTDIST'] = destinetion
+            #перевести дату и время в удобный формат
+            if flightinfo['TPLAN'] is None or flightinfo['DPLAN'] is None:
+                flightinfo['TIMEPLAN'] = None
             else:
-                reiseInfo['TIMEPLAN'] = DT.datetime.strptime(reiseInfo['DPLAN'] + ' ' + reiseInfo['TPLAN'], '%d.%m.%Y %H:%M')
-            if reiseInfo['TEXP'] is None or reiseInfo['DEXP'] is None:
-                reiseInfo['TIMEEXP'] = None
+                flightinfo['TIMEPLAN'] = DT.datetime.strptime(flightinfo['DPLAN'] + ' ' + flightinfo['TPLAN'], '%d.%m.%Y %H:%M')
+            if flightinfo['TEXP'] is None or flightinfo['DEXP'] is None:
+                flightinfo['TIMEEXP'] = None
             else:
-                reiseInfo['TIMEEXP'] = DT.datetime.strptime(reiseInfo['DEXP'] + ' ' + reiseInfo['TEXP'], '%d.%m.%Y %H:%M')
-            if reiseInfo['TFACT'] is None or reiseInfo['DFACT'] is None:
-                reiseInfo['TIMEFACT'] = None
+                flightinfo['TIMEEXP'] = DT.datetime.strptime(flightinfo['DEXP'] + ' ' + flightinfo['TEXP'], '%d.%m.%Y %H:%M')
+            if flightinfo['TFACT'] is None or flightinfo['DFACT'] is None:
+                flightinfo['TIMEFACT'] = None
             else:
-                reiseInfo['TIMEFACT'] = DT.datetime.strptime(reiseInfo['DFACT'] + ' ' + reiseInfo['TFACT'], '%d.%m.%Y %H:%M')
-            if reiseInfo['STATUS'] is None:
-                #departure
+                flightinfo['TIMEFACT'] = DT.datetime.strptime(flightinfo['DFACT'] + ' ' + flightinfo['TFACT'], '%d.%m.%Y %H:%M')
+            self.append(flightinfo)
+            flightinfo = {}
+
+    def handlenullstatus(self, flightsinfo):
+        HOUR2 = DT.timedelta(seconds=7200)
+        MIN40 = DT.timedelta(seconds=2400)
+        MIN20 = DT.timedelta(seconds=1200)
+        DEPARTPLAN = 'Вылет по плану'
+        DEPARTTIMEEXP = 'Вылет по расч. времени '
+        STARTCHEKIN = 'Начинается регистрация пассажиров и багажа'
+        CHECKIN = 'Регистрация. Стойки: '
+        BETWEENCHECKBOARD = 'Регистрация закончена. Загрузка багажа'
+        BOARDING = 'Посадка пассажиров'
+        UPDATETIMEDEPART = 'Уточнение времени вылета'
+        ARRIVEPLAN = 'Прилет ожидается по плану'
+        ARRIVEEXP = 'Прилет по расч. времени '
+        #отработать пустые статусы
+        for flight in self:
+            if flight['STATUS'] is None:
+                fly = flight['FLY']
                 now = DT.datetime.now()
-                if reiseInfo['AD'] == '0':
-                    if reiseInfo['TIMEEXP'] is None:
-                        reiseInfo['STATUS'] = 'Вылет по плану'
+                #departure
+                if flight['AD'] == '0':
+                    if flight['TIMEEXP'] is None:
+                        flight['STATUS'] = DEPARTPLAN
                     else:
-                        if now < reiseInfo['TIMEEXP'] - DT.timedelta(seconds=7200):
-                            reiseInfo['STATUS'] = 'Вылет по расч. времени ' + reiseInfo['TEXP']
-                        elif reiseInfo['TIMEEXP'] - DT.timedelta(seconds=7200) <= now <= reiseInfo['TIMEEXP'] - DT.timedelta(seconds=2400):
-                            reiseInfo['STATUS'] = 'Регистрация пассажиров и багажа'
-                        elif reiseInfo['TIMEEXP'] - DT.timedelta(seconds=2400) < now <= reiseInfo['TIMEEXP']:
-                            reiseInfo['STATUS'] = 'Посадка пассажиров на борт'
+                        if now < flight['TIMEEXP'] - HOUR2 and not flightsinfo.getflightstatus(fly, 'STATCHECKIN'):
+                            flight['STATUS'] = DEPARTTIMEEXP + flight['TEXP']
+                        elif now < flight['TIMEEXP'] - HOUR2 and flightsinfo.getflightstatus(fly, 'STATCHECKIN'):
+                            flight['STATUS'] = CHECKIN + flightsinfo.getflightstatus(fly, 'CHECKIN')
+                        elif flight['TIMEEXP'] - HOUR2 <= now <= flight['TIMEEXP'] - MIN40 and not flightsinfo.getflightstatus(fly, 'STATCHECKIN'):
+                            flight['STATUS'] = STARTCHEKIN
+                        elif flight['TIMEEXP'] - HOUR2 <= now <= flight['TIMEEXP'] - MIN40 and flightsinfo.getflightstatus(fly, 'STATCHECKIN'):
+                            flight['STATUS'] = CHECKIN + flightsinfo.getflightstatus(fly, 'CHECKIN')
+                        elif flight['TIMEEXP'] - MIN40 < now <= flight['TIMEEXP'] - MIN20:
+                            flight['STATUS'] = BETWEENCHECKBOARD
+                        elif flight['TIMEEXP'] - MIN20 < now <= flight['TIMEEXP']:
+                            flight['STATUS'] = BOARDING
                         else:
-                            reiseInfo['STATUS'] = 'Уточнение времени вылета'
+                            flight['STATUS'] = UPDATETIMEDEPART
                 #arrivels
                 else:
-                    if reiseInfo['TIMEEXP'] is None:
-                        reiseInfo['STATUS'] = 'Прилет ожидается по плану'
+                    if flight['TIMEEXP'] is None:
+                        flight['STATUS'] = ARRIVEPLAN
                     else:
-                        reiseInfo['STATUS'] = 'Прилет по расч. времени ' + reiseInfo['TEXP']
-            self.append(reiseInfo)
-            reiseInfo = {}
+                        flight['STATUS'] = ARRIVEEXP + flight['TEXP']
+
+
     def __str__(self):
         st = ''
         for elem in self:
             st += str(elem) + '\n'
         return st
+
     def today(self):
         result = Flights()
         for flight in self:
@@ -139,6 +183,7 @@ class Flights(list):
                     if flight['TIMEPLAN'].date() == DT.datetime.today().date():
                         result.append(flight)
         return result
+
     def yesterday(self):
         result = Flights()
         for flight in self:
@@ -155,6 +200,7 @@ class Flights(list):
                     if flight['TIMEPLAN'].date() == DT.datetime.today().date() - DT.timedelta(days=1):
                         result.append(flight)
         return result
+
     def tomorrow(self):
         result = Flights()
         for flight in self:
@@ -171,18 +217,33 @@ class Flights(list):
                     if flight['TIMEPLAN'].date() == DT.datetime.today().date() + DT.timedelta(days=1):
                         result.append(flight)
         return result
+
     def save(self, filename):
         f = open(filename, 'wb')
         pickle.dump(self, f)
+        return True
+
+    def load(self, filename):
+        try:
+            f = open(filename, 'rb')
+        except FileNotFoundError:
+            return False
+        self = pickle.load(f)
+        return True
+
     def isdifferent(self, picklefile):
         flag = False
-        f = open(picklefile, 'rb')
+        try:
+            f = open(picklefile, 'rb')
+        except FileNotFoundError:
+            return True
         oldinfo = pickle.load(f)
         for old, new in zip(oldinfo, self):
             if old != new:
                 flag = True
                 break
         return flag
+
     def converttoHTML(self, template):
         '''get and dileved template and return string in HTML. Where template is name of file. Template is frie parts: start
         , body whith data and end'''
@@ -197,8 +258,10 @@ class Flights(list):
                 partlines += line
         parts.append(partlines)
         fdiscript.close()
-        if len(parts) != 3:
-            print('Error - !')
+        if len(parts) < 3:
+            raise Exception("Separators in template less then 2. The pattern should be divided into three parts")
+        if len(parts) > 3:
+            raise Exception("Separators in template more then 2. The pattern should be divided into three parts")
         st = ''
         for flight in self:
             st += parts[1].format(**flight)
@@ -210,6 +273,8 @@ def savetofile(str, filename, codepage='utf-8'):
         f = open(filename, 'w', encoding=codepage)
         f.write(str)
         f.close()
+        return True
+
 def sendfilestoftp(filenamelist, server, username=None, password=None):
     for filename in filenamelist:
         ftp = FTP(server, username, password)
@@ -217,11 +282,46 @@ def sendfilestoftp(filenamelist, server, username=None, password=None):
         ftp.storlines('STOR '+filename, file)
         file.close()
         ftp.close()
+    return True
+
+
+def getxmlfromserver(filename, server, port, request):
+    '''запросить xml и сохранить в файл'''
+    if int(sys.version_info[0]) == 3:
+        conector = HTTPConnection(server, port)
+        conector.request("GET", request)
+        result = conector.getresponse()
+        file = open(filename, "w")
+        file.write(result.read().decode("utf-8"))
+        conector.close()
+        file.close()
+    else:
+        raise Exception("Python version less then 3")
+
+
 def getflighttime(flight):
     return flight['TIMEPLAN']
 
 
 if __name__ == '__main__':
+    arrivalxmlreqreg = ('93.157.148.58', 7777, '/pls/apex/f?p=1511:1:0:::NO:LAND,VID:0,0')
+    arrivalxmlfile = 'arrivals.xml'
+    getxmlfromserver(arrivalxmlfile, *arrivalxmlreqreg)
+    arrivals = Flights()
+    arrivals.getfromxml(arrivalxmlfile)
+
+    flightsinfo = FlightsInfo()
+    flightsinfo.updatefromflightes(arrivals)
+    arrivals.handlenullstatus(flightsinfo)
+    if arrivals.isdifferent('arrivals.pkl'):
+        savetofile(arrivals.converttoHTML('templatebdc.php'), 'test.php')
+    print(flightsinfo)
+    flightsinfo.save('arrivalsinfo.pkl')
+    flightsinfo.load('arrivalsinfo.pkl')
+    #for flightinfo in flightsinfo:
+    print(arrivals)
+
+    '''
     arrivalxmlreqreg = ('172.17.10.2', 7777, "/pls/apex/f?p=1515:1:0:::NO:LAND,VID:1,0")
     arrivalxmlreqchart = ('172.17.10.2', 7777, "/pls/apex/f?p=1515:1:0:::NO:LAND,VID:1,1")
     arrivals = Flights()
@@ -249,11 +349,11 @@ if __name__ == '__main__':
     ftablodepart = 'departure.php'
     ftabloarrive = 'arrivals.php'
 
-    '''arrivalsinfo = FlightsInfo()
-    arrivalsinfo.updatefromflightes(arrivals)
-    print(arrivalsinfo)
+    #arrivalsinfo = FlightsInfo()
+    #arrivalsinfo.updatefromflightes(arrivals)
+    #print(arrivalsinfo)
 
-    '''
+
     if departures.isdifferent(departurepicklefile):
         departures.save(departurepicklefile)
         savetofile(departures.today().converttoHTML(templdeparttablo), fsitedepart, 'cp1251')
@@ -274,4 +374,4 @@ if __name__ == '__main__':
         print('Send arrivals')
     else:
         print('No diff on arrivals')
-
+'''
